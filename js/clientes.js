3e34b04336f77b413e585ff08a48b7a2d02d8a31
rogerio-damasio <raugusto2512@gmail.com>
2026-06-03 14:09:44 -0300

    Update clientes.js

--- js/clientes.js
+++ js/clientes.js
@@ -54,7 +54,7 @@ ref.on("value", dados_tabela => {
                 <td>${reg.nome}</td>
                 <td>${reg.email}</td>
                 <td>
-                    <button class="btn btn-danger btn-sm onclick="excluir('${id}')">
+                    <button class="btn btn-danger btn-sm" onclick="excluir('${id}')">
                         <i class="bi bi-trash"></i>
                     </button>
                 </td>
