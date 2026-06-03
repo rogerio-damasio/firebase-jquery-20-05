f09efa9831068bf473d2089969130de3cc696237
rogerio-damasio <raugusto2512@gmail.com>
2026-06-03 14:06:08 -0300

    Fix button onclick and refactor cancelar function

--- js/clientes.js
+++ js/clientes.js
@@ -54,7 +54,7 @@ ref.on("value", dados_tabela => {
                 <td>${reg.nome}</td>
                 <td>${reg.email}</td>
                 <td>
-                    <button class="btn btn-danger btn-sm">
+                    <button class="btn btn-danger btn-sm onclick="excluir('${id}')">
                         <i class="bi bi-trash"></i>
                     </button>
                 </td>
@@ -94,17 +94,25 @@ function editar(id, nome, email){
     $("#status"). text("Editanto registro...");
 }
 
-function cancelar() {
+//Função Cancelar
+function cancelar(){
     idcapturado = null;
     limpar();
     $("#status").text("");
     $("#salvar")
         .text("Salvar")
         .removeClass("btn-success")
-        .addClass("btn-primary");
-    $("#cancelar").hide();
+        .addClass("btn-primary")
+         $("#cancelar").hide();
 }
-
-$("#cancelar").click(function () {
+ 
+$("#cancelar").click(function(){
     cancelar();
 });
+ 
+//Função Excluir
+function excluir(id){
+    if(confirm("Tem certeza que deseja excluir?")){
+            db.ref("clientes/" + id).remove();
+        }
+}
