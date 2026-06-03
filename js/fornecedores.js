f43761bfb0e203e5af39d7947242e766bc0cd408
Miguel Scalet <miguelscalet2000@gmail.com>
2026-06-03 13:56:35 -0300

    Update fornecedores.js

--- js/fornecedores.js
+++ js/fornecedores.js
@@ -104,3 +104,18 @@ function editar(id, nome, cnpj, estado, email){
 
     $("#status"). text("Editanto registro...");
 }
+
+function cancelar() {
+    idcapturado = null;
+    limpar();
+    $("#status").text("");
+    $("#salvar")
+        .text("Salvar")
+        .removeClass("btn-success")
+        .addClass("btn-primary");
+    $("#cancelar").hide();
+}
+
+$("#cancelar").click(function () {
+    cancelar();
+});
